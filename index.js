174dde3f7ab2afa7b81bd99a9cdce8dcf0642b11
laramontana <larabrightside@gmail.com>
2017-11-09 22:36:54 +0300

    Done.

--- index.js
+++ index.js
@@ -14,5 +14,13 @@ const increaseRankBy = function (n) {
 }
 
 const deepestChild = function () {
-  let parent = document.getElementById('grand-node');
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
 }
